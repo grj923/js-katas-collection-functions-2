182d1b2c1e16f41ad3b399331a97de3f1a1d4c2c
Greg Johnson <grj923@gmail.com>
2020-10-22 17:15:35 -0400

    All Katas Complete

--- main.js
+++ main.js
@@ -42,3 +42,45 @@ console.log(add(2, 2));
 console.log(multiply(5, 5));
 console.log(multiply(10, 10));
 console.log(power(4, 2));
+
+// Factorial
+
+console.log("Factorial");
+
+function factorial(a) {
+  count = a;
+  result = 1;
+  while (count > 1) {
+    result = multiply(result, count);
+    count--;
+    console.log(count);
+  }
+  return result;
+}
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(6));
+
+//Fibonacci
+
+console.log("Fibonacci");
+
+function fibonacci(n) {
+  count = 1;
+  sum1 = 0;
+  sum2 = 1;
+  sumTotal = 0;
+  while (count < n) {
+    sumTotal = add(sum1, sum2);
+    sum1 = sum2;
+    sum2 = sumTotal;
+
+    count++;
+    // console.log(count);
+    // console.log(sumTotal);
+  }
+  return sumTotal;
+}
+console.log(fibonacci(8));
+console.log(fibonacci(9));
+console.log(fibonacci(10));
